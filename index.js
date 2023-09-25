966ed5a9fecf615ad48053a90b1de6ea2828f5ae
someart <maissamth@gmail.com>
2023-09-25 21:12:11 +0100

    input value

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
-
-
